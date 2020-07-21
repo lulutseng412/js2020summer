1545fa17b92ad57d2eced379ec47e23f260fc8b1
manlu <lulutseng.development@gmail.com>
2020-07-21 13:57:20 +0800

    W4 主線任務 Vue元件

---
+++ w4_MainMission/all.js
@@ -0,0 +1,106 @@
+import pagination from './pagination.js';
+import modal from './modal.js';
+//全域註冊 分頁元件
+Vue.component('pagination',pagination);
+Vue.component('modal',modal);
+new Vue({
+    el:'#app',
+    data:{
+        products:[],
+        tempProduct: {
+            imageUrl:[]
+        },
+        pagination:{},
+        api:{
+            uuid:'2b3d8952-95e7-4cb1-a50e-78c76f622c8c',
+            path:'https://course-ec-api.hexschool.io/api/'
+        },
+        token:'',
+        isNew:'',
+        loadingBtn:''
+    },
+    methods:{
+        openModal(isNew, item) {//打開彈跳視窗的方法
+            switch (isNew) {
+              case 'new'://新增
+                //清空暫存使用
+                this.tempProduct = {
+                    imageUrl:[]
+                };
+                $('#productsModal').modal('show');//bootstrap的Modal用法
+                break;
+              case 'edit'://編輯
+                this.loadingBtn = item.id;
+                const editUrl = `${this.api.path}${this.api.uuid}/admin/ec/product/${item.id}`;
+                axios.get(editUrl).then(resdata=>{
+                    this.tempProduct = resdata.data.data;
+                    $('#productsModal').modal('show');
+                    this.loadingBtn = '';//清除
+                });
+                break;
+              case 'delete'://刪除
+                this.loadingBtn = item.id;
+                const deleteUrl = `${this.api.path}${this.api.uuid}/admin/ec/product/${item.id}`;
+                axios.get(deleteUrl).then(resdata=>{
+                    this.tempProduct = resdata.data.data;
+                    $('#delproductsModal').modal('show');
+                    this.loadingBtn = '';//清除
+                });
+                break;
+              default:
+                break;
+            }
+        },
+        getProducts(num = 1){
+            const url = `${this.api.path}${this.api.uuid}/admin/ec/products?page=${num}`;
+            axios.get(url)
+            .then(resdata =>{
+                //console.log(resdata);
+                this.products = resdata.data.data;
+                this.pagination = resdata.data.meta.pagination;
+
+                if(this.tempProduct.id){
+                    //先清空避免重複觸發事件
+                    this.tempProduct = {
+                        imageUrl:[]
+                    };
+                }
+
+                $('#productsModal').modal('hide');
+            });
+        },
+        updateProduct(){
+            
+        },
+        delProduct(){
+            //DELETE api/{uuid}/admin/ec/product/{id}
+            var url = `${this.api.path}${this.api.uuid}/admin/ec/products?page=${this.tempProduct.id}`;
+            axios.delete(url).then((resdata)=>{
+                //console.log(res);
+            })
+            .catch(function(msg){
+                console.log('=== deleteData catch ===');
+                console.log(msg);
+            })
+
+            //清空暫存的資料
+            this.tempProduct = {
+                imageUrl:[]
+            };
+
+            //把彈跳視窗隱藏
+            $('#delproductsModal').modal('hide');
+            //重新取得產品列表
+            this.getProducts();
+        }
+    },
+    created(){
+        //取得 token
+        this.token = document.cookie.replace(/(?:(?:^|.*;\s*)token\s*\=\s*([^;]*).*$)|^.*$/, "$1");
+        //將 Token 加入到 Headers 內 當作預設值發送 `Bearer ${this.token}` 是api的規定寫法
+        axios.defaults.headers.common.Authorization = `Bearer ${this.token}`;
+        //重新取得產品列表
+        this.getProducts();
+    }
+
+})
---
+++ w4_MainMission/modal.js
@@ -0,0 +1,107 @@
+export default{
+    template:`<div class="modal-dialog modal-xl" role="document">
+    <div class="modal-content border-0">
+      <div class="modal-header bg-dark text-white">
+        <h5 id="exampleModalLabel" class="modal-title">
+          <span>新增產品</span>
+        </h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <div class="row">
+          <div class="col-sm-4">
+            <div class="form-group">
+              <label for="imageUrl">輸入圖片網址</label>
+              <input id="imageUrl" v-model="tempProduct.imageUrl[0]" type="text" class="form-control"
+                placeholder="請輸入圖片連結">
+            </div>
+            <img class="img-fluid" :src="tempProduct.imageUrl" alt>
+          </div>
+          <div class="col-sm-8">
+            <div class="form-group">
+              <label for="title">標題</label>
+              <input id="title" v-model="tempProduct.title" type="text" class="form-control" placeholder="請輸入標題">
+            </div>
+            <div class="form-group">
+              <label for="content">產品描述</label>
+              <textarea id="description" v-model="tempProduct.content" type="text" class="form-control"
+                placeholder="產品描述" >
+              </textarea>
+            </div>
+            <div class="form-group">
+                <label for="category">分類</label>
+                <input id="category" v-model="tempProduct.category" type="text" class="form-control"
+                  placeholder="請輸入分類" >
+            </div>
+            <div class="form-row">
+              <div class="form-group col-md-6">
+                <label for="origin_price">原價</label>
+                <input id="origin_price" v-model="tempProduct.origin_price" type="number" class="form-control"
+                  placeholder="請輸入原價" >
+              </div>
+              <div class="form-group col-md-6">
+                <label for="price">售價</label>
+                <input id="price" v-model="tempProduct.price" type="number" class="form-control"
+                  placeholder="請輸入售價">
+              </div>
+            </div>
+            <div class="form-group">
+              <label for="description">詳細內容</label>
+              <textarea id="description" v-model="tempProduct.description" type="text" class="form-control"
+                placeholder="請輸入詳細內容" >
+              </textarea>
+            </div>
+            <div class="form-group">
+              <div class="form-check">
+                <input id="is_enabled" v-model="tempProduct.is_enabled" class="form-check-input" type="checkbox"
+                  v-bind:true-value="1" v-bind:false-value="0">
+                <label class="form-check-label" for="is_enabled">是否啟用</label>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-outline-secondary" data-dismiss="modal">
+          取消
+        </button>
+        <button type="button" class="btn btn-primary" @click="updateProduct">
+          確認
+        </button>
+      </div>
+    </div>
+  </div>`,
+    data(){//元件裡的資料使用return
+      return{
+        //tempProduct:{
+        //    imageUrl:[]
+        //},
+        //api:{
+        //    
+        //}
+      }
+    },
+    props:['tempProduct','api'],
+    methods: {
+        updateProduct() {
+            var vm = this;
+            if(vm.tempProduct.id){
+                let urlEdit = `${vm.api.path}${vm.api.uuid}/admin/ec/product/${vm.tempProduct.id}`;
+                //id存在表示是編輯 PATCH api/{uuid}/admin/ec/product/{id}
+                axios.patch(urlEdit,vm.tempProduct).then(resdata=>{
+                    //console.log(resdata);
+                    vm.$emit('updatelist');//呼叫元件外層事件
+                })
+            }else{
+                let urlCreate = `${vm.api.path}${vm.api.uuid}/admin/ec/product`;
+                //id不存在表示是新增 POST api/{uuid}/admin/ec/product
+                axios.post(urlCreate,vm.tempProduct).then(function(res){
+                    //console.log(resdata);
+                    vm.$emit('updatelist');//呼叫元件外層事件
+                })
+            }
+        },
+    },
+}
